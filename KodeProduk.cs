013d51e19d97e5c96dc53ccfbd70bd528967e7e4
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-11 07:18:14 +0700

    menambahkan class KodeProduk

---
+++ KodeProduk.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_103022300043
+{
+    internal class KodeProduk
+    {
+        public String produkElektronik;
+        public String kodeProduk;
+
+        public KodeProduk(String produkElektronik, String kodeProduk)
+        {
+            this.produkElektronik = produkElektronik;
+            this.kodeProduk = kodeProduk;
+        } 
+
+        public String getKodeProduk()
+        {
+            return kodeProduk;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul4_103022300043;
+
+KodeProduk[] listKodeProduk = new KodeProduk[10];
+
+listKodeProduk[0] = new KodeProduk("Laptop", "E100");
+listKodeProduk[1] = new KodeProduk("Smartphone", "E101");
+listKodeProduk[2] = new KodeProduk("Tablet", "E102");
+listKodeProduk[3] = new KodeProduk("Headset", "E103");
+listKodeProduk[4] = new KodeProduk("Keyboard", "E104");
+listKodeProduk[5] = new KodeProduk("Mouse", "E105");
+listKodeProduk[6] = new KodeProduk("Printer", "E106");
+listKodeProduk[7] = new KodeProduk("Monitor", "E107");
+listKodeProduk[8] = new KodeProduk("Smartwatch", "E108");
+listKodeProduk[9] = new KodeProduk("Kamera", "E109");
+
+Console.WriteLine("Produk Elektronik | Kode Produk ");
+
+
+foreach (KodeProduk kodeProduk in listKodeProduk)
+{
+    if (kodeProduk.produkElektronik.Length > 7)
+    {
+        Console.WriteLine(kodeProduk.produkElektronik + "\t\t" + kodeProduk.getKodeProduk());
+    }
+    else
+    {
+        Console.WriteLine(kodeProduk.produkElektronik + "\t\t\t" + kodeProduk.getKodeProduk());
+    }
+}
